b63a99e696302b99dfa5418a1f4b3f994d7ba8c6
CJ Ogbuehi <fahzee1@gmail.com>
2015-12-30 20:23:22 -0600

    added DB info

--- twinning/settings.py
+++ twinning/settings.py
@@ -87,8 +87,10 @@ WSGI_APPLICATION = 'twinning.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE':'django.db.backends.postgresql',
+        'NAME': 'django',
+        'USER':'django',
+        'PASSWORD':'RYWKJ2WjpO'
     }
 }
 
