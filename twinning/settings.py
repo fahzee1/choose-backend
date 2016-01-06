7a5b1d3a1524669a0ff7e2236506e114cad01e2f
CJ Ogbuehi <fahzee1@gmail.com>
2016-01-06 13:38:29 -0500

    added session domain name

--- twinning/settings.py
+++ twinning/settings.py
@@ -104,6 +104,7 @@ CACHES = {
 }
 
 SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
+SESSION_COOKIE_DOMAIN =".api.trychoose.com"
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
