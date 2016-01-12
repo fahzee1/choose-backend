95cc4c2da9837c772944d5d3acb982daf70f9d41
CJ Ogbuehi <fahzee1@gmail.com>
2016-01-12 15:22:08 -0500

    added new relic code

--- twinning/wsgi.py
+++ twinning/wsgi.py
@@ -8,9 +8,13 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "twinning.settings")
-
 application = get_wsgi_application()
+
+try:
+    import newrelic.agent
+    newrelic.agent.initialize('/virtualenvs/choose/twinning/newrelic.ini')
+except ImportError:
+    pass
