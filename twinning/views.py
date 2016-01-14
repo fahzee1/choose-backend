b2b9a2a346a67b10876384a5b309524bb7808a6f
CJ Ogbuehi <fahzee1@gmail.com>
2016-01-14 14:08:11 -0500

    turned off static for now

--- twinning/views.py
+++ twinning/views.py
@@ -12,6 +12,7 @@ def home(request):
 
     elif current_site.domain == 'emekaenterprises.com':
         return render(request,'emeka/index.html',{'foo':'bar'})
+
     else:
-        return HttpBadResponse()
+        return HttpResponseBadRequest()
 
