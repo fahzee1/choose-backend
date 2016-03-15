e4ff8de014bdf3d76cf0e201e72e3c0de1dfebc6
CJ Ogbuehi <fahzee1@gmail.com>
2016-03-15 13:17:52 -0500

    add domain to allowed hosts

--- twinning/settings_destiny.py
+++ twinning/settings_destiny.py
@@ -5,6 +5,8 @@ ALLOWED_HOSTS = ['teamdestinyfoundation.com']
 
 ROOT_URLCONF = 'twinning.urls_destiny'
 
+ALLOWED_HOSTS = ['www.teamdestinyfoundation.com','teamdestinyfoundation.com']
+
 try:
     from local_settings import *
 except ImportError:
