bd70b565b3db31159c78c602021ebe9b2133edf4
CJ Ogbuehi <fahzee1@gmail.com>
2017-02-14 14:12:13 -0600

    added .org to allowed domains

--- twinning/settings_destiny.py
+++ twinning/settings_destiny.py
@@ -1,7 +1,7 @@
 from settings import *
 
 SITE_ID = 3
-ALLOWED_HOSTS = ['teamdestinyfoundation.com','www.teamdestinyfoundation.com']
+ALLOWED_HOSTS = ['teamdestinyfoundation.com','www.teamdestinyfoundation.com','teamdestinyfoundation.org','www.teamdestinyfoundation.org']
 
 ROOT_URLCONF = 'twinning.urls_destiny'
 
